ad92972da3cc7724bb405a651f13189ae461b2ab
hikwoa-hfw <hibbanhfw@gmail.com>
2024-10-27 22:19:32 +0700

    npm run build

--- src/bg.tsx
+++ src/bg.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React from "react";
+
 import { AuroraBackground } from "./components/ui/aurora";
 
 export function AuroraBackgroundDemo() {
@@ -15,15 +15,15 @@ export function AuroraBackgroundDemo() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
+        className="relative flex flex-col items-center justify-center gap-4 px-4"
       >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+        <div className="text-center text-3xl font-bold dark:text-white md:text-7xl">
           Background lights are cool you know.
         </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+        <div className="py-4 text-base font-extralight dark:text-neutral-200 md:text-4xl">
           And this, is chemical burn.
         </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        <button className="w-fit rounded-full bg-black px-4 py-2 text-white dark:bg-white dark:text-black">
           Debug now
         </button>
       </motion.div>
